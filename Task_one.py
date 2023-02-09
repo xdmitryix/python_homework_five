7b366423c2598be15b14aa546021b393da259beb
dmitry <rbdeman@yandex.ru>
2023-02-10 00:41:02 +0300

    сделал первую задачу

--- Task_one.py
+++ Task_one.py
@@ -2,14 +2,18 @@
 # два числа A и B, и возводит число А в целую степень B с
 # помощью рекурсии.
 
-# A = 3; B = 5 -> 243 (3⁵)
+# A = 3; B = 5 -> 243 (3⁵) -> 3*3*3*3*3
 # A = 2; B = 3 -> 8
 
 
 
-
-
-
+def degree(one, two):
+    if two == 1:
+        return one
+    else:
+        return one * degree(one, two-1)
 
 a = int(input('введи число A: '))
 b = int(input('введи число B: '))
+result = degree(a,b)
+print(result)
